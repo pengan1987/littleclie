65b0eb765451863e32c1ebb4f6b41a92befe5286
Andy(Pengan) Zhou <pengan1987@msn.com>
2023-02-23 17:46:01 -0800

    Update for better gopher compatibility

--- channel_gopher.php
+++ channel_gopher.php
@@ -35,23 +35,26 @@ $rss = Feed::loadRss($rssUrlInt);
 $rssTitle = mb_convert_encoding($rss->title, 'gbk', 'UTF-8');
 $items = $rss->item;
 if ($page < 0) {
-    echo 'i' . $rssTitle . "\tnull.host\t1\r\n";
+    echo "1>BACK TO MENU<\t/users/pengan/news\tsdf.org\t70\n";
+    echo 'i' . $rssTitle . "\t\tnull.host\t1\n";
+    echo "i\t\tnull.host\t1\n";
     for ($i = 0; $i < sizeof($items); $i++) {
         $articleTitle = mb_convert_encoding($items[$i]->title, 'gbk', 'UTF-8');
         $dateStr = date('Y-m-d H:i', (int) $items[$i]->timestamp);
-        $paragraphLink = "/users/pengan/cgi-bin/news.cgi?feed=" . $feed . "&p=" . $i;
-        echo "0" . $articleTitle . " " . $dateStr . "\t" . $paragraphLink . "\tsdf.org\t70\r\n";
+        $paragraphLink = "/users/pengan/cgi-bin/news.cgi?" . $feed . "=" . $i;
+        echo "0" . $articleTitle . " " . $dateStr . "\t" . $paragraphLink . "\tsdf.org\t70\n";
 
     }
-
+    echo ".";
 } else {
     $item = $items[$page];
-    $itemTitle = $item->title . "\r\n\r\n";
-    $itemDescription = $itemTitle . $item->description;
+    $itemTitle = $item->title . "\n\n";
+	$itemDescription = html_entity_decode($item->description,ENT_COMPAT, 'UTF-8');
+	$itemDescription = $itemTitle .$itemDescription;
     $itemDescription = convert_img_to_text($itemDescription);
     $itemDescription = preg_replace("/<p[^>]*?>/", "", $itemDescription);
-    $itemDescription = str_replace("</p>", "\r\n", $itemDescription);
-    $itemDescription = str_replace("&nbsp;", " ", $itemDescription);
+    $itemDescription = str_replace("</p>", "\n", $itemDescription);
+    
     $itemDescription = strip_tags($itemDescription);
     $itemDescription = word_wrap_chinese($itemDescription, 60);
     $itemDescription = mb_convert_encoding($itemDescription, 'gbk', 'UTF-8');
